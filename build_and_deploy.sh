49e5e1a9090624fe73f878f086c7abdba141c6c1
Sorin Matei <sorinmatei@Sorins-MacBook-Pro.local>
2023-08-27 10:30:28 +0300

    test2

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Build Docker image
-docker build -t tf-app:v2 .
+docker build -t tf-app:latest .
 
 # Tag the image with version
-docker tag tf-app:v2 sorinmatei/tf-app
+docker tag tf-app:latest sorinmatei/tf-app
 
 # Push the image to Docker Hub
 docker push sorinmatei/tf-app
 
 # Apply Kubernetes deployment using Terraform
 cd CI
-terraform apply -auto-approve
+kubectl get pods
